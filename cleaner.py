ec9e9f6e268d0ec5758a5348059d9be63a9255e8
Jamal Moir <jamal.moir@gmail.com>
2016-02-28 10:46:31 +0900

    Add blob-wrapper removal

--- cleaner.py
+++ cleaner.py
@@ -11,14 +11,19 @@ def main():
 
     # remove the multitude of useless spans
     span_removed = replace_regex(r'<\/?span[^>]*>', '', text)
-    # remove empty tags eg <pre></pre>
-    empty_removed = replace_regex(r'<([^>]*)><\/\1>', '', span_removed)
     # put headings on one line
     heading_fixed = replace_regex(r'<h([1-5])>\n([^<]*)<\/h\1>',
-                                  r'\n<h\1>\2<h\1>', empty_removed)
+                                  r'\n<h\1>\2</h\1>', span_removed)
+    # remove class blob-wrapper divs
+    blob_removed = replace_regex(r'(<div class="blob-wrapper[^>]*>)'
+                                '(.*?(?=<\/div>))(<\/div>)', r'\2',
+                                heading_fixed)
+    # remove empty tags eg <pre></pre>
+    empty_removed = replace_regex(r'<([^>]*)>[\s\n\r]*<\/\1>', '', blob_removed)
+
 
     with open(dirty_file + '_cleaned', 'w+') as f:
-        f.write(heading_fixed)
+        f.write(empty_removed)
 
 def replace_regex(regex, repl, text):
     """Replaces all occurances of a regex pattern regex, in text"""
