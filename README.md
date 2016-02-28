cd82b316cdddc1faf363e367d2e301c817d7e4a9
Jamal Moir <jamal.moir@gmail.com>
2016-02-28 09:01:49 +0900

    Add heading fix functionality

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 A small script to clean up bloggers messy, automatically generated HTML.
 
 ####TODO####
-- Make headings on the same line, on a line of their own
 - Remove the massive blob-wrapper divs
--- cleaner.py
+++ cleaner.py
@@ -8,18 +8,30 @@ def main():
     with open(dirty_file) as f:
         text = f.read()
 
-    markup_removed = purge_regex(r'<\/?span[^>]*>', text)
-    empty_removed = purge_regex(r'<([^>]*)><\/\1>', markup_removed)
+    #remove the multitude of useless spans
+    span_removed = purge_regex(r'<\/?span[^>]*>', text)
+    #remove empty tags eg <pre></pre>
+    empty_removed = purge_regex(r'<([^>]*)><\/\1>', span_removed)
+    #put headings on one line
+    heading_fixed = replace_regex(r'<h([1-5])>\n([^<]*)<\/h\1>',
+    r'\n<h\1>\2<h\1>', empty_removed)
 
     with open(dirty_file + '_cleaned', 'w+') as f:
-        f.write(empty_removed)
+        f.write(heading_fixed)
 
 def purge_regex(regex, text):
-    #removed all occurances of regex pattern in text
+    """Removes all occurances of a regex pattern regex, in text"""
     purger = re.compile(regex, re.DOTALL)
     purged = purger.sub('', text)
 
     return purged
+
+def replace_regex(regex, repl, text):
+    """Replaces all occurances of a regex pattern regex, in text"""
+    replacer = re.compile(regex, re.DOTALL)
+    replaced = replacer.sub(repl, text)
+
+    return replaced
 
 if __name__ == '__main__':
     main()
